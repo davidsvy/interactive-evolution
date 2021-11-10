1dcba4c11480b2373d2a9fef071d58c77b5e6fd1
davidsvy <zuzcus@gmail.com>
2021-11-10 05:36:04 +0200

    Add run.py

---
+++ run.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+from src.ea import Evolutionary_algorithm
+import yaml
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--config', '-c', default='configs/config.yaml',
+        help='Path to the config file')
+
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    img_size = (config['gui_args']['img_width'],
+                config['gui_args']['img_height'])
+
+    config['gui_args'].pop('img_width', None)
+    config['gui_args'].pop('img_height', None)
+    config['gui_args']['img_size'] = img_size
+
+    return config
+
+
+def main():
+    config = parse_args()
+    ea = Evolutionary_algorithm(
+        gan_args=config['gan_args'],
+        gui_args=config['gui_args'],
+        **config['ea_args'])
+    ea.run()
+
+
+if __name__ == '__main__':
+    main()
